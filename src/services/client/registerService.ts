16acdb81539d174e94826658a1249f247efcca57
jhainethv161 <126474850+vnaranjo161@users.noreply.github.com>
2024-07-12 21:31:58 -0500

    import correction

--- src/services/client/registerService.ts
+++ src/services/client/registerService.ts
@@ -1,13 +1,12 @@
 import registerRepository from "../../repository/user/client/registerRepository";
 import UserDto from "../../DTO/user/userDto";
 import generateHash from "../../helpers/generateHash";
-import RegisterRepository from "../../repository/user/admin/registerAdviser";
 
 class RegisterService{
     static async register(user:UserDto){
         const password: string = await generateHash(user.passwordC);
         user.passwordC = password;
-        return await RegisterRepository.registerAdviser(user);
+        return await registerRepository.register(user);
     }
 }
 export default RegisterService;
