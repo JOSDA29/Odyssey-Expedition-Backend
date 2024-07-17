39429243b0a74af9e21d7d0b8ad40fbe2dc3e445
Luis <luiseduardovelez881@gmail.com>
2024-07-17 09:42:33 -0500

    Token corrections

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from "express";
 import AuthService from "../services/AuthService";
 import Auth from "../DTO/authDTO";
-import { setTokenCookie } from "../middlewares/cookieMiddleware";
 
 let auth = async (req: Request, res: Response) => {
     try {
         const {email, password} = req.body;
         
         const token: any = await  AuthService.auth(new Auth(email, password));
-        
-        setTokenCookie(res, token)
         if (token) {
-            setTokenCookie(res, token)
+
+            //Guardar el token en localstorage
+            localStorage.setItem('AccessToken', token)
+
             return res.status(200).json({
                 status: 'Successful authentication',
                 AccessToken: token
