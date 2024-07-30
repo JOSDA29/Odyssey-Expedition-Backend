5aa12226503410fdb40af3377944b0858c618943
jhainethv161 <126474850+vnaranjo161@users.noreply.github.com>
2024-07-30 07:23:53 -0500

    modification route

--- src/routes/user/client/clientRoutes.ts
+++ src/routes/user/client/clientRoutes.ts
@@ -5,6 +5,8 @@ import getByEmail from '../../../controllers/user/client/getByEmail';
 import register from '../../../controllers/user/client/registerController';
 import updateDataController from '../../../controllers/user/client/update';
 import uploadImage from '../../../controllers/user/client/uploadImage';
+import changeStateController from '../../../controllers/user/client/changeState';
+
 
 import {
     validatorParamsEmail,
@@ -25,6 +27,11 @@ import {
     validsParametersUpdate,
     validatorUpdate,
 } from '../../../middlewares/updateValidator';
+
+import {
+    changeStateValidator, 
+    validator 
+} from '../../../middlewares/changeStateValidator';
 
 
 import validateToken from '../../../middlewares/validateTokenMiddleware';
@@ -65,6 +72,12 @@ router.get('/getImage', validateToken(['Client']), getImage);
 
 router.post('/register', validatorParamsRegister, validatorRegister, register);
 
+router.put('/',
+     validateToken(['Client']),
+     changeStateValidator,
+     validator,
+     changeStateController
+);
 
 router.put(
     '/update',
