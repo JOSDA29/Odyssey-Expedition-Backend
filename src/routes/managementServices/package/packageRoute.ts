fb48b104d37f7c08a0db240a6a9cb0b725e9b33c
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2024-08-17 15:35:46 -0500

    Filter PACKAGE

--- src/routes/managementServices/package/packageRoute.ts
+++ src/routes/managementServices/package/packageRoute.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import validateToken from '../../../middlewares/validateTokenMiddleware';
 import { validatorParamsPackage, validator } from '../../../middlewares/routes validators/package/createValidator';
 import { validatorParamsPackageUpdate, validatorUpdate } from '../../../middlewares/routes validators/package/updateValidator';
+import { validatorParamsFilter, validatorFilter } from '../../../middlewares/routes validators/package/filterValidator';
 import upload from '../../../middlewares/multerMiddleware';
-
 
 import createController from '../../../controllers/managementServices/package/create';
 import uploadImageController from '../../../controllers/managementServices/package/uploadImage';
 import updateController from '../../../controllers/managementServices/package/update';
 import deleteController from '../../../controllers/managementServices/package/delete';
+import filterController from '../../../controllers/managementServices/package/filter';
 
 
 
@@ -21,5 +22,7 @@ router.put('/updateImage', validateToken(['Adviser']), upload.single('file'), up
 router.put('/update', validateToken(['Adviser']), validatorParamsPackageUpdate, validatorUpdate, updateController);
 
 router.delete('/delete/:id', validateToken(['Adviser']), deleteController);
+
+router.get('/filter', validateToken(['Adviser']), validatorParamsFilter, validatorFilter, filterController);
 
 export default router;
