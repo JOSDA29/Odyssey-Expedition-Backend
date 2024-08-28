b2273dabe5315ad14c7b51719e3101157974de5b
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2024-08-28 17:59:54 -0500

    Update Client Corrections

--- src/services/user/client/client.ts
+++ src/services/user/client/client.ts
@@ -4,8 +4,16 @@ import ClientR from '../../../repository/user/client/updateData';
 
 class Client {
     static async updateC(user: User) {
-        const client  = await ClientR.update(user);
-        return { message: client?.message };
+        try {
+            const result = await ClientR.update(user);
+            if(result === 0) {
+                throw new Error("No record was updated. Verify that the Email is correct.");
+            }
+            return { success: true, message: 'Successfull Update.'};
+        } catch (error) {
+            console.error('Error updating admin: ', error);
+            throw new Error('internal Error when trying to update the admin');
+        }
     }
     static async changePassword(changePassword: ChangePassword) {
         const client = await ClientR.changePassword(changePassword);
