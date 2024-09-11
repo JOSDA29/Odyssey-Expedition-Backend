f0e5d24c21f6e68b1bd417de73fdba41d1ef7c12
jhainethv161 <126474850+vnaranjo161@users.noreply.github.com>
2024-09-11 14:53:51 -0500

    modification in connection to the database

--- src/config/configDB.ts
+++ src/config/configDB.ts
@@ -1,5 +1,6 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -9,6 +10,10 @@ const connection = new Pool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     max: 10,
+    ssl: {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync(process.env.DB_SSL_CA || './DigiCertGlobalRootG2.crt.pem').toString(), 
+    },
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
 });
